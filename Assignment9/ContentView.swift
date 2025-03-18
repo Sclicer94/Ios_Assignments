cf8d631068dd8c30f61926b376e817d97e684c73
Omar Mauldin <W0650036@myscc.ca>
2025-03-18 17:10:20 -0400

    Finished Campsite Class

--- Assignment9/ContentView.swift
+++ Assignment9/ContentView.swift
@@ -5,9 +5,28 @@
 //  Created by Nathaniel Mauldin on 2025-03-18.
 //
 
+
+//This is a single view applications.  Your main screen will have a search bar and this will populate a list of television shows that are returned from the search.
+//The list will contain:
+//the shows name
+//the episode name
+//a truncated description
+//display the provided episode image.
+//The main screen will include a title of your name and shows - for example "Ed's Shows"
+//When you click on a row it will take you to a details page which will include the following:
+//The episode title
+//An image
+//The show name
+//the full episode description
+//An image that represents whether the show is explicit or not
+//A link that will display additional web details - this comes from the trackViewUrl
+//And any other information that you want to include from the API
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    let shows = Bundle.main.decode("rickAndMorty.json")
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ Assignment9/Data/Show.swift
@@ -0,0 +1,62 @@
+//
+//  Show.swift
+//  Assignment9
+//
+//  Created by Nathaniel Mauldin on 2025-03-18.
+//
+
+import Foundation
+
+struct Shows: Codable{
+    var data:[Show]
+}
+
+class Show: Codable, Hashable, Identifiable {
+    
+    static var show = Show(trackId:756852895, artistName:"Rick and Morty",trackName:"Pilot",shortDescription:"Rick moves in with his daughter's family and establishes himself as a bad influence on his grandson", longDescription:"Rick moves in with his daughter's family and establishes himself as a bad influence on his grandson, Morty.",artworkUrl100:"https://is1-ssl.mzstatic.com/image/thumb/Video113/v4/74/30/fd/7430fd41-956d-349f-fa2e-3e627623b6dd/pr_source.png/100x100bb.jpg",collectionViewUrl:"https://itunes.apple.com/us/tv-season/pilot/id741096885?i=756852895&uo=4")
+    
+    static func == (lhs: Show, rhs: Show) -> Bool {
+        lhs.trackId == rhs.trackId
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(trackId)
+    }
+    
+    var trackId : Int
+    var artistName : String
+    var trackName: String
+    var shortDescription: String
+    var longDescription : String
+    var artworkUrl100:String
+    var collectionViewUrl: String?
+    
+    init(trackId: Int, artistName: String, trackName: String, shortDescription: String, longDescription: String, artworkUrl100: String, collectionViewUrl: String? = nil) {
+        self.trackId = trackId
+        self.artistName = artistName
+        self.trackName = trackName
+        self.shortDescription = shortDescription
+        self.longDescription = longDescription
+        self.artworkUrl100 = artworkUrl100
+        self.collectionViewUrl = collectionViewUrl
+    }
+}
+
+
+extension Bundle{
+    func decode(_ file: String) -> [Show]{
+        
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Can't find a file with this name")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Can't convert to data")
+        }
+        
+        guard let shows = try? JSONDecoder().decode(Shows.self, from: data) else {
+            fatalError("Can't decode the json")
+        }
+        
+        return shows.data
+    }
+}
